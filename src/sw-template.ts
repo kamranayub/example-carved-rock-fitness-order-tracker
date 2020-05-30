e6ada831725c91f251aed6fb875a6a3d9839a891
Kamran Ayub <kamran.ayub@gmail.com>
2020-05-30 14:55:16 -0500

    Add NetworkFirst strategy for API backend calls

--- src/sw-template.ts
+++ src/sw-template.ts
@@ -1,11 +1,13 @@
-import { clientsClaim } from 'workbox-core';
-import { NavigationRoute, registerRoute } from 'workbox-routing';
-import { precacheAndRoute, createHandlerBoundToURL } from 'workbox-precaching';
+import { clientsClaim } from "workbox-core";
+import { NavigationRoute, registerRoute } from "workbox-routing";
+import { precacheAndRoute, createHandlerBoundToURL } from "workbox-precaching";
+import { NetworkFirst } from "workbox-strategies";
+import { CacheableResponsePlugin } from "workbox-cacheable-response";
 
 declare let self: ServiceWorkerGlobalScope;
 
-self.addEventListener('message', function(event: ExtendableMessageEvent) {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
+self.addEventListener("message", function (event: ExtendableMessageEvent) {
+  if (event.data && event.data.type === "SKIP_WAITING") {
     self.skipWaiting();
   }
 });
@@ -17,7 +19,25 @@ precacheAndRoute(self.__WB_MANIFEST);
 const handler = createHandlerBoundToURL(`${process.env.PUBLIC_URL}/index.html`);
 
 const navigationRoute = new NavigationRoute(handler, {
-  denylist: [new RegExp('^/_'), new RegExp('/[^/?]+\\.[^/]+$')],
+  denylist: [new RegExp("^/_"), new RegExp("/[^/?]+\\.[^/]+$")],
 });
 
 registerRoute(navigationRoute);
+
+//
+// Attempt to retrieve latest orders, otherwise fallback to cached
+// responses.
+//
+registerRoute(
+  ({ url }) =>
+    url.origin === "https://carved-rock-fitness-backend.azurewebsites.net",
+  new NetworkFirst({
+    networkTimeoutSeconds: 5,
+    cacheName: "orders",
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  })
+);
