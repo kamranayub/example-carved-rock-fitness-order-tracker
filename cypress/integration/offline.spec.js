70b2c2e86381ced44dee795f013ae0c4f3426b81
Kamran Ayub <kamran.ayub@gmail.com>
2020-06-22 23:37:00 -0500

    prep for 4.9.0

--- cypress/integration/offline.spec.js
+++ cypress/integration/offline.spec.js
@@ -3,51 +3,57 @@ describe("offline support", () => {
     cy.visit("/");
   });
 
-  it.only("should wait for service worker to be registered", () => {
-    // let installation process occur
-    cy.wait(3000);
-
+  it("should wait for service worker to be registered", () => {
     // we have a hook that adds an HTML classname when service worker is ready
-    cy.get("html").should("have.class", "sw-ready");
+    cy.get("html", { timeout: 6000 }).should("have.class", "sw-ready");
   });
 
   it("should load orders when online", () => {
     cy.findByText("Order #1001").should("be.visible");
   });
 
-  it("should show toast when browser goes offline", () => {
-    cy.offline();
-    cy.get("ion-toast:not(.overlay-hidden)")
-      .should("be.visible")
-      .should((e) => {
-        const [dom] = e.get();
-        const message = dom.shadowRoot.querySelector(".toast-message");
+  describe("when going offline", () => {
+    beforeEach(() => {
+      cy.offline();
+    });
 
-        expect(message).to.have.text(
-          "Looks like you went offline, your data may not be up-to-date."
-        );
-      });
+    it("should show toast when browser goes offline", () => {
+      cy.get("ion-toast:not(.overlay-hidden)")
+        .should("be.visible")
+        .should((e) => {
+          const [dom] = e.get();
+          const message = dom.shadowRoot.querySelector(".toast-message");
+
+          expect(message).to.have.text(
+            "Looks like you went offline, your data may not be up-to-date."
+          );
+        });
+    });
+
+    it("should navigate to order details using cache", () => {
+      cy.findByText("Order #1001").click();
+      cy.findByText("Backpacker's Guide to Colorado Hiking Trails").should(
+        "be.visible"
+      );
+    });
   });
 
-  it("should navigate to order details using cache", () => {
-    cy.offline();
-    cy.findByText("Order #1001").click();
-    cy.findByText("Backpacker's Guide to Colorado Hiking Trails").should(
-      "be.visible"
-    );
-  });
+  describe("when coming back online", () => {
+    beforeEach(() => {
+      cy.online();
+    });
 
-  it("should show toast when browser comes back online and refetch order data", () => {
-    cy.online();
-    cy.get("ion-toast:not(.overlay-hidden)")
-      .should("be.visible")
-      .should((e) => {
-        const [dom] = e.get();
-        const message = dom.shadowRoot.querySelector(".toast-message");
+    it("should show toast", () => {
+      cy.get("ion-toast:not(.overlay-hidden)")
+        .should("be.visible")
+        .should((e) => {
+          const [dom] = e.get();
+          const message = dom.shadowRoot.querySelector(".toast-message");
 
-        expect(message).to.have.text(
-          "You're back online, your data is automatically being updated."
-        );
-      });
+          expect(message).to.have.text(
+            "You're back online, your data is automatically being updated."
+          );
+        });
+    });
   });
 });
