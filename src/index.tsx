eaef760ef87392c5d12e5981448539a54e0c0008
Kamran Ayub <kamran.ayub@gmail.com>
2020-06-22 22:06:38 -0500

    Add `sw` HTML classes

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,14 @@ ReactDOM.render(<App />, document.getElementById("root"));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+serviceWorker.register({
+  onSuccess: (registration) => {
+    document.getElementsByName('html')[0].classList.add('sw', 'sw-registered');
+  },
+  onUpdate: (registration) => {
+    document.getElementsByName('html')[0].classList.add('sw', 'sw-updated');
+  }
+});
 
 //
 // For static files outside webpack/React
