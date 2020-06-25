ea92a2472f947eb6dea68ad97dc354eb6c6d1433
Kamran Ayub <kamran.ayub@gmail.com>
2020-06-25 00:06:47 -0500

    Remove pause

--- cypress/integration/offline.spec.js
+++ cypress/integration/offline.spec.js
@@ -4,7 +4,6 @@ describe("offline support", () => {
   });
 
   it("should wait for service worker to be registered", () => {
-    cy.pause();
     // we have a hook that adds an HTML classname when service worker is ready
     cy.get("html", { timeout: 6000 }).should("have.class", "sw-ready");
   });
