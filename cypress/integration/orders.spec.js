5522f1c0328aa8c8f8202a198cd0adbe64a12e23
Kamran Ayub <kamran.ayub@gmail.com>
2020-07-11 22:09:00 -0500

    step 1

--- cypress/integration/orders.spec.js
+++
@@ -1,9 +0,0 @@
-describe("orders", () => {
-  before(() => {
-    cy.visit("/");
-  });
-
-  it("should show orders list", () => {
-    cy.findByText("Order #1001").should("be.visible");
-  });
-});
--- cypress/integration/responsive-design.spec.js
+++ cypress/integration/responsive-design.spec.js
@@ -1,46 +1,18 @@
 describe("responsive design", () => {
-  //
-  // To reference Ionic default breakpoints, see:
-  // https://ionicframework.com/docs/layout/css-utilities#ionic-breakpoints
-  //
-  // To reference Cypress presets, see:
-  // https://docs.cypress.io/api/commands/viewport.html#Arguments
-  //
-
   describe("on phones", () => {
     before(() => {
       cy.visit("/");
-      cy.findByText("Order #1001").should("be.visible");
     });
 
     ["portrait", "landscape"].forEach((orientation) => {
       describe(`in ${orientation}`, () => {
-        it("should display menu navigation button", () => {
-          cy.viewport("iphone-6", orientation);
-          cy.get("ion-menu-button").should("be.visible");
-        });
+        it("should display menu navigation button", () => {});
 
-        it("should open left menu using menu button navigation", () => {
-          cy.viewport("iphone-6", orientation);
-          cy.get("ion-menu-button").click();
-          cy.get("ion-list").findByText("My Orders").should("be.visible");
-          cy.waitForIonicAnimations();
-        });
+        it("should open left menu using menu button navigation", () => {});
 
-        it("should close left menu when clicking off", () => {
-          cy.viewport("iphone-6", orientation);
-          cy.reload();
-          cy.get("ion-menu-button").click();
-          cy.waitForIonicAnimations();
-          cy.get('ion-menu[role="navigation"]')
-            .click("topRight")
-            .should("not.be.visible");
-        });
+        it("should close left menu when clicking off", () => {});
 
-        it("should not display marketing imagery", () => {
-          cy.viewport("iphone-6", orientation);
-          cy.get(".hero-image-col").should("not.be.visible");
-        });
+        it("should not display marketing imagery", () => {});
       });
     });
   });
@@ -50,22 +22,11 @@ describe("responsive design", () => {
       cy.visit("/");
     });
 
-    it("should not display left menu", () => {
-      cy.viewport("ipad-2");
-      cy.get('ion-menu[role="navigation"]').should("not.be.visible");
-      cy.get("ion-menu-button").should("be.visible");
-    });
+    it("should not display left menu", () => {});
 
-    it("should be the first size to show marketing imagery", () => {
-      cy.viewport("ipad-2");
-      cy.get(".hero-image-col").should("be.visible");
-    });
+    it("should be the first size to show marketing imagery", () => {});
 
-    it("should display left menu in landscape", () => {
-      cy.viewport("ipad-2", "landscape");
-      cy.get('ion-menu[role="navigation"]').should("be.visible");
-      cy.get("ion-menu-button").should("not.be.visible");
-    });
+    it("should display left menu in landscape", () => {});
   });
 
   describe("on larger screens", () => {
@@ -73,9 +34,6 @@ describe("responsive design", () => {
       cy.visit("/");
     });
 
-    it("should continue displaying marketing imagery", () => {
-      cy.viewport("macbook-15");
-      cy.get(".hero-image-col").should("be.visible");
-    });
+    it("should continue displaying marketing imagery", () => {});
   });
 });
