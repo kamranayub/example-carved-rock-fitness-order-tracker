13536f5d393e4b18505a69e96c0497f92a133b58
Kamran Ayub <kamran.ayub@gmail.com>
2020-07-29 00:02:38 -0500

    silent logs, comment out ff again

--- e2e/wdio.conf.js
+++ e2e/wdio.conf.js
@@ -82,7 +82,7 @@ exports.config = {
   // Define all options that are relevant for the WebdriverIO instance here
   //
   // Level of logging verbosity: trace | debug | info | warn | error | silent
-  // logLevel: "info",
+  logLevel: process.env.CI ? "silent" : "info",
   // outputDir: path.resolve(__dirname, "wdio", "logs"),
   // screenshotDir: path.resolve(__dirname, "wdio", "screenshots"),
   //
