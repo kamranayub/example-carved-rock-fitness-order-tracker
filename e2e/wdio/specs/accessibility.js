45dddfa9648413381996d8dde6bdaf72a22a3049
Kamran Ayub <kamran.ayub@gmail.com>
2020-08-10 20:30:48 -0500

    docs: a11y

--- e2e/wdio/specs/accessibility.js
+++ e2e/wdio/specs/accessibility.js
@@ -1,8 +1,15 @@
+/**
+ * Accessibility test suite
+ *
+ * Covers keyboard navigation for non-touch devices
+ */
 const { isRealMobileDevice } = require("../util");
 
 describe("accessibility", () => {
   before(() => {
     browser.url("/");
+
+    // Wait for orders to be loaded and displayed
     const ordersList = $("[data-testid='orders-list']");
     expect(ordersList).toBeDisplayed();
   });
@@ -10,6 +17,11 @@ describe("accessibility", () => {
   !isRealMobileDevice() &&
     describe("keyboard navigation", () => {
       it("should focus on My Orders menu item when tabbing first time", () => {
+        // Tabs to the first menu item
+        //
+        // This bit can be flaky and possibly writing a utility to "tabUntil"
+        // the desired element is focused could work more reliably. I've noticed
+        // when this fails, it seems like the tab index is off by one.
         browser.keys("Tab");
         const focused = browser.focused();
 
@@ -18,6 +30,7 @@ describe("accessibility", () => {
       });
 
       it("should focus on first order next", () => {
+        // This will focus on the first order in the list
         browser.keys("Tab");
         const focused = browser.focused();
 
@@ -25,17 +38,29 @@ describe("accessibility", () => {
       });
 
       it("should navigate to order when selected with Enter key", () => {
+        // Hitting "enter" will activate the link
         browser.keys("Enter");
+
+        // Make sure we navigated to the right page
         expect(browser).toHaveUrl(
           new URL("orders/1001", browser.config.baseUrl).toString()
         );
+
+        // Wait for any order data to load
         const title = $("ion-title*=Order #1001");
         expect(title).toBeDisplayed();
       });
 
       it("should be able to focus on notification toggle", () => {
-        browser.pause(500); // wait for Ionic page transition
+        // Wait for Ionic page transition
+        // without this, the test will be quite flaky
+        browser.pause(500);
+
+        // Tabbing twice, once on the back button,
+        // the next onto the toggle button
         browser.keys(["Tab", "Tab"]);
+
+        // Ensure the toggle button is what has focus
         const focused = browser.focused();
         expect(focused).toHaveAttribute(
           "aria-label",
